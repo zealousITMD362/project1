dd7f4d3d16b247f81cd2cfeb7f09ff285de78cf8
zeal0u5 <122125451+zeal0u5@users.noreply.github.com>
2023-10-08 13:22:19 -0500

    added alert and style for submit

--- js/site.js
+++ js/site.js
@@ -1,3 +1,7 @@
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
 $(document).ready(function () {
   $("#submit").click(function () {
     alert("You have been signed up for the gift");
