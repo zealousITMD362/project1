7b6ec21617aad696bfa160aa4b2b1698396733d6
zeal0u5 <122125451+zeal0u5@users.noreply.github.com>
2023-10-27 15:03:43 -0500

    added validation and changed submit

--- js/site.js
+++ js/site.js
@@ -7,3 +7,7 @@ $(document).ready(function () {
     alert("You have been signed up for the gift! Here it is: a compliment. You are very cool!");
   });
 });
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+ } 
